df6204f3aab028829dd7a9b167d13137b9fcb641
yorung <yorung@gmail.com>
2015-02-01 23:32:56 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
+int main(int argc, char* argv[])
+{
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	lua_close(L);
+	return 0;
+}
