c8832fedd878efba43a9bd68084bb777ae64e2c1
yorung <yorung@gmail.com>
2015-02-01 23:48:31 +0900

    simple binding

--- main.cpp
+++ main.cpp
@@ -3,10 +3,31 @@ extern "C" {
 #include <lauxlib.h>
 #include <lualib.h>
 }
+
+static lua_State *L;
+
+static int Printer(lua_State *L)
+{
+	printf("test\n");
+	return 0;
+}
+
+static void Bind()
+{
+	lua_register(L, "Printer", Printer);
+}
+
 int main(int argc, char* argv[])
 {
-	lua_State *L = luaL_newstate();
+	L = luaL_newstate();
 	luaL_openlibs(L);
+	Bind();
+	luaL_dostring(L, "Printer()");
+
+	int top = lua_gettop(L);
+	if (top > 0) {
+		printf("error! stack is not empty\n");
+	}
 	lua_close(L);
 	return 0;
 }
