aecb3e4c943c2122076877362a125a8465212040
yorung <yorung@gmail.com>
2015-02-04 00:22:38 +0900

    dump global variables

--- main.lua
+++ main.lua
@@ -14,3 +14,16 @@ local m = getmetatable(obj)
 for k,v in pairs(m) do
 	print(k, v)
 end
+
+storedItIntoGlobal = "stored string in global"
+
+local cnt = 0
+for k, v in pairs(_G) do
+	print(k, v)
+	cnt = cnt + 1
+end
+
+
+
+print("there are "..tostring(cnt).." global variables" )
+print("length of _G: "..tostring(#_G))
