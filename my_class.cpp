477bb59f0b61691a2a1503ea40c4ea911ac1caac
yorung <yorung@gmail.com>
2015-02-15 21:27:15 +0900

    placement new test

--- my_class.cpp
+++ my_class.cpp
@@ -1,11 +1,15 @@
 #include "af_lua_helpers.h"
 
+#include <new>
+
 extern lua_State *L;
 
 
 class MyClass
 {
 public:
+	MyClass() { puts("MyClass::MyClass called"); }
+	~MyClass() { puts("MyClass::~MyClass called"); }
 	int value;
 };
 
@@ -14,38 +18,31 @@ namespace
 	char myClassName[] = "MyClass";
 }
 
+#define GET_MYCLASS \
+	MyClass* p = (MyClass*)luaL_checkudata(L, 1, myClassName); \
+	if (!p) { return 0; }
+
 static int MyClassSetValue(lua_State *L)
 {
+	GET_MYCLASS
 	DumpStack();
-	MyClass** pp = (MyClass**)luaL_checkudata(L, -2, myClassName);
-	(*pp)->value = (int)lua_tointeger(L, -1);
+	p->value = (int)lua_tointeger(L, -1);
 	return 0;
 }
 
 static int MyClassGetValue(lua_State *L)
 {
+	GET_MYCLASS
 	DumpStack();
-	MyClass** pp = (MyClass**)luaL_checkudata(L, -1, myClassName);
-	lua_pushinteger(L, (*pp)->value);
+	lua_pushinteger(L, p->value);
 	return 1;
-}
-
-template <class MyClass, const char* myClassName> static int AnyTypeGC(lua_State* L)
-{
-	DumpStack();
-	MyClass** pp = (MyClass**)luaL_checkudata(L, -1, myClassName);
-	delete *pp;
-	DumpStack();
-	return 0;
-
 }
 
 static int MyClassNew(lua_State *L)
 {
-	MyClass** pp = (MyClass**)lua_newuserdata(L, sizeof(MyClass*));
+	MyClass* p = new (lua_newuserdata(L, sizeof(MyClass))) MyClass;
 	DumpStack();
-	*pp = new MyClass;
-	(*pp)->value = 0;
+	p->value = 0;
 	luaL_getmetatable(L, myClassName);
 	DumpStack();
 	lua_setmetatable(L, -2);
@@ -67,7 +64,7 @@ void BindMyClass()
 
 	static struct luaL_Reg methods[] =
 	{
-		{"__gc", AnyTypeGC<MyClass, myClassName>},
+		{ "__gc", [](lua_State* L) { GET_MYCLASS p->~MyClass(); return 0; } },
 		{"SetValue", MyClassSetValue},
 		{"GetValue", MyClassGetValue},
 		{nullptr, nullptr},
