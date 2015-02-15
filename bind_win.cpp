2373a05626119b01e355f5e1a69627e3b68a8a0a
yorung <yorung@gmail.com>
2015-02-15 12:39:20 +0900

    __index and __newindex must be a function to access C++ member variables from Lua

--- bind_win.cpp
+++ bind_win.cpp
@@ -17,11 +17,52 @@ static const char* myClassName = "RECT";
 
 static int RECTToString(lua_State *L)
 {
+	GET_RECT
 	char buf[64];
-	RECT* r = (RECT*)luaL_checkudata(L, -1, myClassName);
-	sprintf_s(buf, sizeof(buf), "(%f, %f, %f, %f)", r->left, r->top, r->right, r->bottom);
+	sprintf_s(buf, sizeof(buf), "(%d, %d, %d, %d)", r->left, r->top, r->right, r->bottom);
 	lua_pushstring(L, buf);
 	return 1;
+}
+
+static int RECTIndex(lua_State *L)
+{
+	GET_RECT
+	const char* key = lua_tostring(L, 2);
+	DumpStack();
+	if (!strcmp(key, "left")) {
+		lua_pushnumber(L, r->left);
+	}
+	else if (!strcmp(key, "top")) {
+		lua_pushnumber(L, r->top);
+	}
+	else if (!strcmp(key, "right")) {
+		lua_pushnumber(L, r->right);
+	}
+	else if (!strcmp(key, "bottom")) {
+		lua_pushnumber(L, r->bottom);
+	}
+	else {
+		return 0;
+	}
+	return 1;
+}
+
+static int RECTNewIndex(lua_State *L)
+{
+	GET_RECT
+	const char* key = lua_tostring(L, 2);
+	LONG val = (LONG)lua_tointeger(L, 3);
+	DumpStack();
+	if (!strcmp(key, "left")) {
+		r->left = val;
+	} else if (!strcmp(key, "top")) {
+		r->top = val;
+	} else if (!strcmp(key, "right")) {
+		r->right = val;
+	} else if (!strcmp(key, "bottom")) {
+		r->bottom = val;
+	}
+	return 0;
 }
 
 static int RECTNew(lua_State *L)
@@ -50,20 +91,14 @@ void BindWin()
 	assert(r);
 	DumpStack();
 
-	lua_pushstring(L, "__index");
-	lua_newtable(L);
 	static struct luaL_Reg indexMethods[] =
 	{
-		{ "left", [](lua_State* L) { GET_RECT lua_pushinteger(L, r->left); return 1; } },
-		{ "top", [](lua_State* L) { GET_RECT lua_pushinteger(L, r->top); return 1; } },
-		{ "right", [](lua_State* L) { GET_RECT lua_pushinteger(L, r->right); return 1; } },
-		{ "bottom", [](lua_State* L) { GET_RECT lua_pushinteger(L, r->bottom); return 1; } },
+		{ "__index", RECTIndex },
+		{ "__newindex", RECTNewIndex },
+		{ "__tostring", RECTToString },
 		{ nullptr, nullptr },
 	};
 	luaL_setfuncs(L, indexMethods, 0);
-	DumpStack();
-
-	lua_settable(L, 1);
 	DumpStack();
 
 	lua_pop(L, 1);
--- bind_win.lua
+++ bind_win.lua
@@ -1,3 +1,5 @@
 local r = RECT(100, 150, 200, 250)
 
-print(r:top(), r:bottom())
+r.right = 1000
+print(r.left, r.top, r.right, r.bottom)
+print(r)
