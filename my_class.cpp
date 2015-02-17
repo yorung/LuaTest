2ea53de13d44e0c5230e55c696b5f3750e076d3f
yorung <yorung@gmail.com>
2015-02-18 01:29:40 +0900

    simplified binding

--- my_class.cpp
+++ my_class.cpp
@@ -4,45 +4,31 @@
 
 extern lua_State *L;
 
-
 class MyClass
 {
 public:
-	MyClass() { puts("MyClass::MyClass called"); }
-	~MyClass() { puts("MyClass::~MyClass called"); }
+	MyClass()
+	{
+		value = 0;
+		puts("MyClass::MyClass called");
+	}
+	~MyClass()
+	{
+		puts("MyClass::~MyClass called");
+	}
 	int value;
 };
 
-namespace
-{
-	char myClassName[] = "MyClass";
-}
+static const char* myClassName = "MyClass";
 
 #define GET_MYCLASS \
 	MyClass* p = (MyClass*)luaL_checkudata(L, 1, myClassName); \
 	if (!p) { return 0; }
 
-static int MyClassSetValue(lua_State *L)
-{
-	GET_MYCLASS
-	DumpStack();
-	p->value = (int)lua_tointeger(L, -1);
-	return 0;
-}
-
-static int MyClassGetValue(lua_State *L)
-{
-	GET_MYCLASS
-	DumpStack();
-	lua_pushinteger(L, p->value);
-	return 1;
-}
-
 static int MyClassNew(lua_State *L)
 {
 	MyClass* p = new (lua_newuserdata(L, sizeof(MyClass))) MyClass;
 	DumpStack();
-	p->value = 0;
 	luaL_getmetatable(L, myClassName);
 	DumpStack();
 	lua_setmetatable(L, -2);
@@ -65,9 +51,9 @@ void BindMyClass()
 	static struct luaL_Reg methods[] =
 	{
 		{ "__gc", [](lua_State* L) { GET_MYCLASS p->~MyClass(); return 0; } },
-		{"SetValue", MyClassSetValue},
-		{"GetValue", MyClassGetValue},
-		{nullptr, nullptr},
+		{ "SetValue", [](lua_State* L) { GET_MYCLASS p->value = (int)lua_tointeger(L, -1); return 0; } },
+		{ "GetValue", [](lua_State* L) { GET_MYCLASS lua_pushinteger(L, p->value); return 1; } },
+		{ nullptr, nullptr },
 	};
 	luaL_setfuncs(L, methods, 0);
 	DumpStack();
