1c046f323dc8b96d158237fb4ef3232b41550e52
yorung <yorung@gmail.com>
2015-02-18 15:06:10 +0900

    dump any name of lua_State

--- af_lua_helpers.h
+++ af_lua_helpers.h
@@ -8,8 +8,10 @@
 void _dumpStack(lua_State* L, const char* func, int line);
 #ifdef _DEBUG
 #define DumpStack() _dumpStack(L, __FUNCTION__, __LINE__)
+#define DumpStackL(L) _dumpStack(L, __FUNCTION__, __LINE__)
 #else
 #define DumpStack()
+#define DumpStackL(L)
 #endif
 
 void BindClass(lua_State* L, const char* className, luaL_Reg methods[], lua_CFunction creator);
