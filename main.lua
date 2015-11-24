6ee1c8b1a43b46d85d37b47c5bbc07270fea575c
yorung <yorung@gmail.com>
2015-11-24 13:24:50 +0900

    test of require's return value

--- main.lua
+++ main.lua
@@ -24,8 +24,13 @@ DumpLoaded()
 require "bind_win"
 require "mes_box"
 require "vec4"
-local obj = dofile("my_class.lua")
-print("Get my value from main.lua", obj:GetValue())
+local obj1 = require("my_class")
+obj1:SetValue(5678)
+local obj2 = require("my_class")
+local obj3 = dofile("my_class.lua")
+print("Get my value of obj1 from main.lua", obj1:GetValue())
+print("Get my value of obj2 from main.lua", obj2:GetValue())
+print("Get my value of obj3 from main.lua", obj3:GetValue())
 DumpLoaded()
 
 print("--------------------------Dump package.searchers")
